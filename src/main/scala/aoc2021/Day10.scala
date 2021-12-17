e2d77eecbb6e1f419792e17aef2fbf8553d13ad5
ajenkinski <adam@thejenkins.org>
2021-12-16 21:03:18 -0500

    Day 10 part 2

--- src/main/scala/aoc2021/Day10.scala
+++ src/main/scala/aoc2021/Day10.scala
@@ -1,16 +1,18 @@
 package aoc2021
 
+// Solution to https://adventofcode.com/2021/day/10
+
 object Day10 extends AOCDay {
   override type InputT = Seq[String]
 
   type Pos = Int
 
   enum SyntaxCheckResult:
-    case Incomplete
+    case Completed(added: String)
     case Error(pos: Pos, char: Char)
     case Ok(pos: Pos)
 
-  val delims = Map(
+  val closeDelims = Map(
     '(' -> ')',
     '[' -> ']',
     '{' -> '}',
@@ -21,27 +23,25 @@ object Day10 extends AOCDay {
     input.linesIterator.toSeq
 
   def checkSyntax(line: String): SyntaxCheckResult =
+    import SyntaxCheckResult._
+
     def checkSyntaxInner(line: String, pos: Pos): SyntaxCheckResult =
-      import SyntaxCheckResult._
       if pos >= line.length then
         Ok(pos)
       else
-        val ch = line(pos)
-        if delims.contains(ch) then
-          checkSyntaxInner(line, pos + 1) match {
-            case Ok(newPos) if newPos >= line.length => Incomplete
-            case Ok(newPos) =>
-              if delims(ch) == line(newPos) then
-                checkSyntaxInner(line, newPos + 1)
-              else
-                Error(newPos, line(newPos))
+        closeDelims.get(line(pos)) match {
+          case Some(closeDelim) => checkSyntaxInner(line, pos + 1) match {
+            case Ok(newPos) if newPos >= line.length => Completed(closeDelim.toString)
+            case Completed(added) => Completed(added + closeDelim)
+            case Ok(newPos) if closeDelim == line(newPos) => checkSyntaxInner(line, newPos + 1)
+            case Ok(newPos) => Error(newPos, line(newPos))
             case other => other
           }
-        else
-          Ok(pos)
+          case None => Ok(pos)
+        }
 
     checkSyntaxInner(line, 0) match {
-      case SyntaxCheckResult.Ok(pos) if pos < line.length => SyntaxCheckResult.Error(pos, line(pos))
+      case Ok(pos) if pos < line.length => Error(pos, line(pos))
       case other => other
     }
 
@@ -53,16 +53,31 @@ object Day10 extends AOCDay {
       '>' -> 25137
     )
 
-    input.iterator.map { line =>
-      checkSyntax(line) match {
-        case SyntaxCheckResult.Error(_, ch) => delimScores(ch)
-        case _ => 0
-      }
+    input.iterator.map(checkSyntax).collect {
+      case SyntaxCheckResult.Error(_, ch) => delimScores(ch)
     }.sum
+
+  // need to return Long to avoid overflow
+  def solvePart2(input: InputT): Long =
+    val delimScores = Map(
+      ')' -> 1,
+      ']' -> 2,
+      '}' -> 3,
+      '>' -> 4
+    )
+
+    val scores = input.map(checkSyntax).collect {
+      case SyntaxCheckResult.Completed(added) => added.foldLeft(0L) { (score, ch) =>
+        score * 5 + delimScores(ch)
+      }
+    }.sorted
+
+    scores(scores.length / 2)
 
   def main(args: Array[String]): Unit = {
     val input = parseInputFile("day10.txt")
 
     println(s"Solution to part 1 = ${solvePart1(input)}")
+    println(s"Solution to part 2 = ${solvePart2(input)}")
   }
 }
