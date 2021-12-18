7d4c047530b33f8930191ae066f49baa6df9d041
ajenkinski <adam@thejenkins.org>
2021-12-17 20:01:02 -0500

    Updated Day 11 to use new immutable Grid type

--- src/main/scala/aoc2021/Day11.scala
+++ src/main/scala/aoc2021/Day11.scala
@@ -3,81 +3,47 @@ package aoc2021
 // Solution to https://adventofcode.com/2021/day/9
 
 object Day11 extends AOCDay {
-  override type InputT = Grid
+  override type InputT = Grid[Int]
 
-  type Grid = Array[Array[Int]]
-
-  val neighborOffsets = Seq((-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1))
-
-  extension (g: Grid)
-    def numRows: Int = g.length
-    def numCols: Int = g(0).length
-    def rowIndices: Range = g.indices
-    def colIndices: Range = g(0).indices
-    def coords: Iterable[(Int, Int)] = new Iterable[(Int, Int)] {
-      override def iterator: Iterator[(Int, Int)] =
-        for r <- rowIndices.iterator; c <- colIndices.iterator yield (r, c)
-    }
-
-    def definedAt(row: Int, col: Int): Boolean = g.isDefinedAt(row) && g(row).isDefinedAt(col)
-    def neighborCoords(row: Int, col: Int): Seq[(Int, Int)] =
-      neighborOffsets
-        .map { case (dr, dc) => (row + dr, col + dc) }
-        .filter { case (nr, nc) => definedAt(nr, nc) }
-
-    def display: Unit =
-      println(g.map(_.mkString).mkString("\n"))
-
-    def deepClone: Grid = g.map(_.clone)
-
-  override def parseInput(input: String): Grid =
-    input.linesIterator
-      .map(line => line.split("").map(_.toInt).toArray)
-      .toArray
+  override def parseInput(input: String): InputT =
+    Grid.from(input.linesIterator
+      .map(line => line.split("").map(_.toInt).toSeq).toSeq)
 
   // single steps the grid, and returns the number of flashes that occurred during the step
-  def singleStepGrid(grid: Grid): Int =
-    var flashed = Set.empty[(Int, Int)]
+  def singleStepGrid(grid: InputT): (InputT, Int) =
+    var flashed = Set.empty[Coord]
 
     // first increment all elements by 1
-    for (row, col) <- grid.coords do
-      grid(row)(col) += 1
+    var grid2 = grid.mapGrid(_ + 1)
 
     while
       var prevFlashed = flashed
-      for (row, col) <- grid.coords do
-        if grid(row)(col) >= 10 && !flashed.contains((row, col)) then
-          flashed += (row, col)
-          for (nrow, ncol) <- grid.neighborCoords(row, col) do
-            grid(nrow)(ncol) += 1
+      for coord <- grid2.coords do
+        if grid2(coord) > 9 && !flashed.contains(coord) then
+          flashed += coord
+          grid2 = grid2.allNeighbors(coord).foldLeft(grid2) { (grid, ncoord) =>
+            grid.updated(ncoord, grid(ncoord) + 1)
+          }
       prevFlashed ne flashed
     do ()
 
     // finally set any flashed locations back to 0
-    for (row, col) <- grid.coords do
-      if grid(row)(col) >= 10 then
-        grid(row)(col) = 0
+    grid2 = grid2.mapGrid(x => if x > 9 then 0 else x)
 
-    flashed.size
+    (grid2, flashed.size)
 
-  def solvePart1(grid: Grid): Int =
-    val gridCopy = grid.deepClone
-    var totalFlashed = 0
+  def solvePart1(grid: InputT): Int =
+    Iterator.unfold(grid) {
+      grid => Some(singleStepGrid(grid).swap)
+    }.take(100).sum
 
-    for _ <- 1 to 100 do
-      totalFlashed += singleStepGrid(gridCopy)
-
-    totalFlashed
-
-  def solvePart2(grid: Grid): Int =
-    val gridCopy = grid.deepClone
-
-    var index = 0
-    while !gridCopy.coords.forall { case (row, col) => gridCopy(row)(col) == 0 } do
-      singleStepGrid(gridCopy)
-      index += 1
-
-    index
+  def solvePart2(grid: InputT): Int =
+    Iterator.unfold(grid) { grid =>
+      if grid.coords.forall(c => grid(c) == 0) then
+        None
+      else
+        Some((grid, singleStepGrid(grid)._1))
+    }.length
 
   def main(args: Array[String]): Unit =
     val input = parseInputFile("day11.txt")
--- src/main/scala/aoc2021/Grid.scala
+++ src/main/scala/aoc2021/Grid.scala
@@ -6,24 +6,21 @@ extension [A](g: Grid[A])
   def numRows: Int = g.length
   def numCols: Int = g(0).length
 
-  def coords: Iterable[Coord] = new Iterable[Coord] {
-    override def iterator: Iterator[Coord] =
-      for r <- g.indices.iterator; c <- g(0).indices.iterator yield Coord(r, c)
-  }
-  
-  def isDefinedAt(c: Coord): Boolean = g.isDefinedAt(c.row) && g(0).isDefinedAt(c.col)
-  
+  def coords: Iterable[Coord] =
+    for r <- g.indices.view; c <- g(0).indices.view yield Coord(r, c)
+
+  def isDefinedAt(c: Coord): Boolean = g.isDefinedAt(c.row) && g(c.row).isDefinedAt(c.col)
+
   def axisNeighbors(c: Coord): Seq[Coord] = Coord.axisOffsets.map(_ + c).filter(isDefinedAt(_))
   def allNeighbors(c: Coord): Seq[Coord] = Coord.allOffsets.map(_ + c).filter(isDefinedAt(_))
 
   def apply(coord: Coord): A = g(coord.row)(coord.col)
 
-  def updated(row: Int, col: Int, newVal: A): Grid[A] =
-    g.updated(row, g(row).updated(col, newVal))
+  def updated(c: Coord, newVal: A): Grid[A] =
+    g.updated(c.row, g(c.row).updated(c.col, newVal))
 
   def mapGrid[B](f: A => B): Grid[B] = g.map(_.map(f))
-  
+
 object Grid:
   def apply[A](rows: Int, cols: Int, fillVal: A): Grid[A] = Vector.fill(rows, cols)(fillVal)
-  def from[A](source: Seq[Seq[A]]): Grid[A] = Vector.from(source.map(_.toVector))
-  
+  def from[A](source: Iterable[Iterable[A]]): Grid[A] = Vector.from(source.map(_.toVector))
