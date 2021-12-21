b58a1e8eb55ce393b574da80c9434209af28f518
ajenkinski <adam@thejenkins.org>
2021-12-20 21:04:44 -0500

    Day 16 cleanup

--- src/main/scala/aoc2021/Day16.scala
+++ src/main/scala/aoc2021/Day16.scala
@@ -15,16 +15,24 @@ object Day16 extends AOCDay {
 
   trait Packet {
     val version: Int
-    val typeId: Int
   }
 
-  case class Literal(version: Int, typeId: Int, value: Long) extends Packet
+  case class Literal(version: Int, value: Long) extends Packet
   case class Operator(version: Int, typeId: Int, children: Seq[Packet]) extends Packet
 
   // I use a parser combinator library to create a packet parser
   class PacketParser extends RegexParsers:
-    def version: Parser[Int] = raw"\d{3}".r ^^ { Integer.parseInt(_, 2) }
-    def typeId: Parser[Int] = raw"\d{3}".r ^^ { Integer.parseInt(_, 2) }
+    // create a parser that parsers an int from the given number of bits
+    def int(nbits: Int): Parser[Int] = repN(nbits, ".".r) ^^ {
+      bits => Integer.parseInt(bits.mkString, 2)
+    }
+
+    // create a parser that succeeds if p succeeds and the given predicate on the result of p returns true
+    def acceptIf[T](p: Parser[T], pred: T => Boolean): Parser[T] =
+      p ^? { case x if pred(x) => x }
+
+    def version: Parser[Int] = int(3)
+    def typeId: Parser[Int] = int(3)
 
     // Bits for one digit other than the last digit of a literal
     def literalGroup: Parser[String] = raw"1\d{4}".r ^^ { _.tail.toString }
@@ -33,24 +41,19 @@ object Day16 extends AOCDay {
     def literalValue: Parser[Long] = rep(literalGroup) ~ literalEndGroup ^^ {
       case parts ~ lastPart => java.lang.Long.parseLong(parts.mkString + lastPart, 2)
     }
-    def literalPacket: Parser[Packet] = version ~ (typeId ^? { case n if n == 4 => n }) ~ literalValue ^^ {
-      case v ~ t ~ value => Literal(v, t, value)
+    def literalPacket: Parser[Packet] = version ~ acceptIf(typeId, _ == 4) ~ literalValue ^^ {
+      case v ~ t ~ value => Literal(v, value)
     }
 
-    // number of bits in an operator packet
-    def numBits: Parser[Int] = raw"\d{15}".r ^^ { Integer.parseInt(_, 2) }
-    // number of packets in an operator packet
-    def numPackets: Parser[Int] = raw"\d{11}".r ^^ { Integer.parseInt(_, 2) }
-
-    def operatorPacket: Parser[Packet] = version ~ (typeId ^? { case n if n != 4 => n }) ~ operatorPacketPayload ^^ {
+    def operatorPacket: Parser[Packet] = version ~ acceptIf(typeId, _ != 4) ~ operatorPacketPayload ^^ {
       case v ~ t ~ children => Operator(v, t, children)
     }
 
     // payload where the number of packets is specified
-    def numPacketsPayload: Parser[Seq[Packet]] = "1" ~> numPackets >> { n => repN(n, packet )}
+    def numPacketsPayload: Parser[Seq[Packet]] = "1" ~> int(11) >> { repN(_, packet ) }
 
     // payload where number of bits is specified
-    def numBitsPayload: Parser[Seq[Packet]] = "0" ~> numBits >> { n => repN(n, ".".r) } ^^ { bits =>
+    def numBitsPayload: Parser[Seq[Packet]] = "0" ~> int(15) >> { repN(_, ".".r) } ^^ { bits =>
       parse(rep1(packet), bits.mkString) match {
         case Success(packets, _) => packets
         case e => throw new Exception(s"Got error while parsing bits payload: $e")
@@ -72,7 +75,7 @@ object Day16 extends AOCDay {
   def foldPackets[A](packet: Packet, accum: A)(f: (A, Packet) => A): A =
     val newAccum = f(accum, packet)
     packet match {
-      case Literal(_, _, _) => newAccum
+      case Literal(_, _) => newAccum
       case Operator(_, _, children) => children.foldLeft(newAccum)((a, packet) => foldPackets(packet, a)(f))
     }
 
@@ -82,17 +85,17 @@ object Day16 extends AOCDay {
 
   def evaluatePacket(packet: Packet): Long =
     packet match {
-      case Literal(_, _, value) => value
+      case Literal(_, value) => value
       case Operator(_, typeId, packets) =>
-        val values = packets.map(evaluatePacket)
+        val args = packets.map(evaluatePacket)
         typeId match {
-          case 0 => values.sum
-          case 1 => values.product
-          case 2 => values.min
-          case 3 => values.max
-          case 5 => if values(0) > values(1) then 1 else 0
-          case 6 => if values(0) < values(1) then 1 else 0
-          case 7 => if values(0) == values(1) then 1 else 0
+          case 0 => args.sum
+          case 1 => args.product
+          case 2 => args.min
+          case 3 => args.max
+          case 5 => if args(0) > args(1) then 1 else 0
+          case 6 => if args(0) < args(1) then 1 else 0
+          case 7 => if args(0) == args(1) then 1 else 0
           case _ => throw new Exception(s"Unexpected typeId: $typeId")
         }
     }
